56e9875fc067f837abe69999e788a3af7808abb2
mtycz <38988445+mtycz@users.noreply.github.com>
2024-08-11 00:46:32 +0800

    Add files via upload

---
+++ TK.js
@@ -0,0 +1,3 @@
+// TK.js
+export const globalUrl = 'https://api.zskj.us.kg';
+export const globalAuthorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsInJvbGVOYW1lIjoicl9yYWYzMXZha24zNSIsImlhdCI6MTcyMzA0MTE5OSwiZXhwIjozMzI4MDY0MTE5OX0.bv4uU0nys-0M2IvQHSxq6hwfgPr5nwF109Hcg_kn4Bg';
--- com.js
+++ com.js
@@ -1,38 +1,36 @@
 
-
 // 汉堡菜单效果
 function toggleMenu() {
-				var menu = document.getElementById("menu");
-				menu.classList.toggle("active");
-				$('.header').addClass('scrolled');
-				const hamburger = document.querySelector('.hamburger-menu');
-				menu.classList.toggle('hidden');
-				hamburger.classList.toggle('active');
-			}
-			
-//共用header引入			
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("active");
+  $('.header').addClass('scrolled');
+  const hamburger = document.querySelector('.hamburger-menu');
+  menu.classList.toggle('hidden');
+  hamburger.classList.toggle('active');
+}
+
+// 共用 header 引入
 async function loadHeader() {
-      try {
-        const response = await fetch('header.html');
-        if (response.ok) {
-          const content = await response.text();
-          const div = document.createElement('div');
-          div.innerHTML = content;
-          document.body.insertBefore(div, document.body.firstChild);
+  try {
+    const response = await fetch('header.html');
+    if (response.ok) {
+      const content = await response.text();
+      const div = document.createElement('div');
+      div.innerHTML = content;
+      document.body.insertBefore(div, document.body.firstChild);
 
-          // 获取当前页面的路径
-          var currentPage = window.location.pathname;
+      // 获取当前页面的路径
+      var currentPage = window.location.pathname;
 
-          // 更通用的判断是否为 index 相关页面
-          if (!currentPage.endsWith('/index.html') &&!currentPage.endsWith('/index.htm') &&!currentPage.endsWith('/index.php')) {
-            div.querySelector('.header').classList.add('scrolled');
-          }
-
-        } else {
-          console.error('Failed to load header');
-        }
-      } catch (error) {
-        console.error('Error loading header:', error);
+      // 更通用的判断是否为 index 相关页面
+      if (!currentPage.endsWith('/index.html') &&!currentPage.endsWith('/index.htm') &&!currentPage.endsWith('/index.php')) {
+        div.querySelector('.header').classList.add('scrolled');
       }
+
+    } else {
+      console.error('Failed to load header');
     }
-	
+  } catch (error) {
+    console.error('Error loading header:', error);
+  }
+}
